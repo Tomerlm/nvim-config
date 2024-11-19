c6ef9dff614f041cccae832cf4060883c27c4983
Tomer Landesman <tomerlm007@gmail.com>
2024-11-20 00:06:46 +0200

    lala

--- lua/tomerlm/init.lua
+++ lua/tomerlm/init.lua
@@ -1,5 +1,34 @@
 require("tomerlm.remap")
 require("tomerlm.set")
-require("tomerlm.packer")
+require("tomerlm.lazy_init")
+
+vim.env.PATH = vim.env.PATH .. '/usr/bin/node'
+
 vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
+vim.cmd([[colorscheme catppuccin]])
+
+local augroup = vim.api.nvim_create_augroup
+local TomerGroup = augroup('Tomer', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+autocmd('LspAttach', {
+    group = TomerGroup,
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    end
+})
